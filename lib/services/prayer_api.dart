128144db4765e304d4cd045c510d3a8ecbd3d2c8
MOMEN56 <5momenalaa5@gmail.com>
2024-11-17 16:42:58 +0200

    make time 12 not 24

--- lib/services/prayer_api.dart
+++ lib/services/prayer_api.dart
@@ -1,34 +1,40 @@
-
 import 'package:dio/dio.dart';
 
 class PrayerApi {
-  final Dio dio;
+  final Dio _dio;
 
-  PrayerApi(this.dio);
+  PrayerApi(this._dio);
 
   Future<Map<String, String>> fetchPrayerTimes(String date, String latitude, String longitude) async {
-    try {
-      final response = await dio.get(
-        'https://api.aladhan.com/v1/timings/$date',
-        queryParameters: {
-          'latitude': latitude,
-          'longitude': longitude,
-          'method': '5', // طريقة حساب أوقات الصلاة
-        },
-      );
+    final String url = 'https://api.aladhan.com/v1/timings/$date'; // تحديد التاريخ
+    final Map<String, dynamic> queryParameters = {
+      'latitude': latitude,  // إحداثيات خط العرض
+      'longitude': longitude, // إحداثيات خط الطول
+      'method': 5, // طريقة الحساب (حسب الطريقة المصرية)
+    };
 
-      var data = response.data['data']['timings'];
-      return {
-        "Fajr": data['Fajr'],
-        "Dhuhr": data['Dhuhr'],
-        "Asr": data['Asr'],
-        "Maghrib": data['Maghrib'],
-        "Isha": data['Isha'],
-        "Sunrise": data['Sunrise'],
-      };
-    } catch (e) {
-      print('Error fetching prayer times: $e');
-      rethrow;
+    try {
+      final response = await _dio.get(url, queryParameters: queryParameters); // استخدام _dio هنا
+
+      if (response.statusCode == 200) {
+        final data = response.data['data']['timings'];
+        return {
+          'Fajr': data['Fajr'],
+          'Sunrise': data['Sunrise'],
+          'Dhuhr': data['Dhuhr'],
+          'Asr': data['Asr'],
+          'Maghrib': data['Maghrib'],
+          'Isha': data['Isha'],
+        };
+      } else {
+        throw Exception('فشل في جلب أوقات الصلاة');
+      }
+    } on DioError catch (e) {
+      if (e.response != null) {
+        throw Exception('خطأ: ${e.response?.statusCode}, ${e.response?.data}');
+      } else {
+        throw Exception('خطأ: ${e.message}');
+      }
     }
   }
 }
